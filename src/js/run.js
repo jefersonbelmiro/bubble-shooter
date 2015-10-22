b8b6e61170357b5dc6848dacd1462d57384fc401
Jeferson Belmiro <jeferson.belmiro@gmail.com>
2015-10-22 21:06:30 -0200

    fixed

--- src/js/run.js
+++ src/js/run.js
@@ -1,6 +1,6 @@
 ;(function(exports) {
 
-    var game = new Phaser.Game(BubbleShoot.UI.width, BubbleShoot.UI.height, Phaser.CANVAS);
+    var game = new Phaser.Game(BubbleShoot.UI.width, BubbleShoot.UI.height, Phaser.AUTO);
     game.state.add('boot', Boot);
     game.state.add('preloader', Preloader);
     game.state.add('menu', Menu);
